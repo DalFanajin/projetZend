62cb3b5696156dd4722879dcaa25ff9af3d64c5a
DalFanajin <l.dantoni@free.fr>
2015-05-19 14:50:49 +0200

    Connexion BD OK

---
+++ config/autoload/db.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+        'aliases' => array(
+            'db' => 'Zend\Db\Adapter\Adapter',
+        ),
+    ),
+    'db' => array(
+        'driver'    => 'pdo',
+        'dsn'       => 'mysql:dbname=test;host=localhost',
+        'username'  => '',
+        'password'  => '',
+    	'driver_options' => array(
+                        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+         ),
+    ),
+);
--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -8,14 +8,25 @@
  */
 
 namespace Application\Controller;
-
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-
+use Zend\Debug\Debug;
+// use Zend\Db\Adapter\Adapter;
+// use Zend\Db\Sql\Sql;
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+    	$adapter = $this->getServiceLocator()->get('db');
+    	Debug::dump($adapter);
+    	$sql = 'INSERT INTO TEST VALUES (2,3);';
+    	$statement = $adapter->createStatement($sql);
+    	$result = $statement->execute();
+//     	$this->getServiceLocator()->get('db');
+//     	Debug::dump($this->getServiceLocator()->get('db'));
+//     	$this->dbAdapter->query('INSERT INTO TEST VALUES (2,3);');
+    	
         return new ViewModel();
     }
+    
 }
